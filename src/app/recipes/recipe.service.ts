507e7ff738c497a5ea215ba5cc9a5fa028e456e5
Khadija Daruwala <khadija.genius@gmail.com>
2020-05-20 11:36:41 +0530

    Using service for cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,15 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is a test recipe', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg'),
     new Recipe('Another Test Recipe', 'This is a test recipe', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg')
   ];
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
 }
