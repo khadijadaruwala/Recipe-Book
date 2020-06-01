1184d4fa3a19875040b0af4e3602cf1eb0ac4cad
Khadija <khadija.genius@gmail.com>
2020-06-01 10:33:15 +0530

    Added template driven form for shopping list edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,19 +9,25 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  // @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  // @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
   // @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  // onAddItem() {
+  //   const ingName = this.nameInputRef.nativeElement.value;
+  //   const ingAmount = this.amountInputRef.nativeElement.value;
+  //   const newIngredient = new Ingredient(ingName, ingAmount);
+  //   this.slService.addIngredient(newIngredient)
+  //   this.ingredientAdded.emit(newIngredient);
+  // }
+
+  onAddItem(form: NgForm) {
+    const formValue = form.value;
+    const newIngredient = new Ingredient(formValue.name, formValue.amount);
     this.slService.addIngredient(newIngredient)
-    // this.ingredientAdded.emit(newIngredient);
   }
 }
