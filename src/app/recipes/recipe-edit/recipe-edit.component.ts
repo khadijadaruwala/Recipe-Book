4089d599390ff8463e40fa84f35f8e73d12c6880
Khadija <khadija.genius@gmail.com>
2020-06-01 16:25:28 +0530

    Fixing bugs - Recipe service in proper position - Delete ingredient form recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -87,6 +87,10 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   getControls() {
     return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
