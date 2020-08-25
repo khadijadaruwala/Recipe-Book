e1eae86e96b71e10bd38ea74620cc22680224349
KhadijaDaruwala <khadija.genius@gmail.com>
2020-08-25 18:48:52 +0530

    Handle loading spinner and error message

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
   idToken: string;
@@ -24,6 +26,17 @@ export class AuthService {
     };
     return this.http.post<AuthResponseData>
       ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBwSs31P6_GzQUBNffzu3RFQmFR9vPkM0o',
-        params);
+        params)
+      .pipe(catchError(errorRes => {
+        let errorMessage = 'An error occured';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
+      }));
   }
 }
