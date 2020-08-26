4e16f245cc4c79b939c3d133861d5c7ad78fbb0c
KhadijaDaruwala <khadija.genius@gmail.com>
2020-08-26 18:58:05 +0530

    Handle auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -54,6 +54,27 @@ export class AuthService {
       }));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpiration: string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpiration)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -67,6 +88,7 @@ export class AuthService {
       token,
       expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
