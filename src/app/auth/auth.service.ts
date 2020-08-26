3d7836941556418c4c5f6126e63dec293ba6c05e
KhadijaDaruwala <khadija.genius@gmail.com>
2020-08-26 18:19:00 +0530

    Handling logout and redirection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
   idToken: string;
@@ -12,14 +13,13 @@ export interface AuthResponseData {
   localId: string;
   registered?: boolean;
 }
-
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
   user = new BehaviorSubject<User>(null);
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   signUp(email: string, password: string) {
     const params = {
@@ -52,6 +52,11 @@ export class AuthService {
           resData.idToken,
           +resData.expiresIn);
       }));
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 
   private handleAuthenticate(email: string, userId: string, token: string, expiresIn: number) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -26,6 +26,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.dataStoreService.fetchRecipes().subscribe();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.userSub.unsubscribe();
   }
