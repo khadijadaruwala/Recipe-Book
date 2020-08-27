7c0b5ac13b30435eb567491f6d7045b58e34191b
KhadijaDaruwala <khadija.genius@gmail.com>
2020-08-27 09:35:20 +0530

    Adding auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,8 @@ export interface AuthResponseData {
 
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  tokenExpirationTimer: any;
+
   constructor(private http: HttpClient, private router: Router) { }
 
   signUp(email: string, password: string) {
@@ -59,25 +61,42 @@ export class AuthService {
       email: string;
       id: string;
       _token: string;
-      _tokenExpiration: string
+      _tokenExpirationDate: string;
     } = JSON.parse(localStorage.getItem('userData'));
     if (!userData) {
       return;
     }
+
     const loadedUser = new User(
       userData.email,
       userData.id,
       userData._token,
-      new Date(userData._tokenExpiration)
+      new Date(userData._tokenExpirationDate)
     );
+
     if (loadedUser.token) {
       this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuthenticate(email: string, userId: string, token: string, expiresIn: number) {
@@ -88,6 +107,7 @@ export class AuthService {
       token,
       expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
